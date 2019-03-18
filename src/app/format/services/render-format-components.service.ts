8b5ebcfebd0ee61c5b564f30a94adbc6b3a310d2
Michail Malykov <michail.malykov@gmail.com>
2019-03-18 16:08:03 +0800

    feat(format): add RenderFormatComponentsService
    Service is used for dynamic render format component according to ENABLED_FORMAT_TYPES (format-types.ts)
    For adding new format or component used should add all proper types to format-types.ts and update ENABLED_FORMAT_TYPES array for enable it in view

---
+++ src/app/format/services/render-format-components.service.ts
@@ -0,0 +1,59 @@
+import { ComponentFactory, ComponentFactoryResolver, Injectable, ViewContainerRef } from '@angular/core';
+import { ENABLED_FORMAT_TYPES, FORMAT, IFormatComponent } from './format-types';
+
+import { ApplyStyleFormatButtonComponent } from '../components/apply-style-format-button/apply-style-format-button.component';
+import { ApplyReplaceFormatDropdownButtonComponent } from '../components/apply-replace-format-dropdown-button/apply-replace-format-dropdown-button.component';
+
+
+@Injectable()
+export class RenderFormatComponentsService {
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+  ) {
+  }
+
+  public render(viewContainerRef) {
+    viewContainerRef.clear();
+
+    ENABLED_FORMAT_TYPES.forEach(format => this.renderFormatComponent(viewContainerRef, format));
+  }
+
+  private renderFormatComponent(viewContainerRef: ViewContainerRef, format: FORMAT) {
+    switch (format) {
+      case FORMAT.BOLD:
+      case FORMAT.ITALIC:
+      case FORMAT.UNDERLINE: {
+        return this.createComponent(
+          viewContainerRef,
+          this.componentFactoryResolver.resolveComponentFactory(ApplyStyleFormatButtonComponent),
+          format
+        );
+      }
+      case FORMAT.COLOR: // TODO: change to color picker
+        return this.createComponent(
+          viewContainerRef,
+          this.componentFactoryResolver.resolveComponentFactory(ApplyStyleFormatButtonComponent),
+          format
+        );
+      case FORMAT.SYNONYMUS:
+        return this.createComponent(
+          viewContainerRef,
+          this.componentFactoryResolver.resolveComponentFactory(ApplyReplaceFormatDropdownButtonComponent),
+          format
+        );
+    }
+
+
+  }
+
+  private createComponent(viewContainerRef: ViewContainerRef, componentFactory: ComponentFactory<any>, format: FORMAT) {
+    const componentRef = viewContainerRef.createComponent(componentFactory);
+
+    (componentRef.instance as IFormatComponent).format = format;
+
+    componentRef.changeDetectorRef.detectChanges();
+  }
+
+
+}
