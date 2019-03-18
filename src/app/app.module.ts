5368a536350d183e2f5a9d69935b05144151d1db
Michail Malykov <michail.malykov@gmail.com>
2019-03-18 16:12:29 +0800

    fix(core): connect missing components/services to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule, MatOptionModule, MatSelectModule, MatSnackBarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { FileComponent } from './file/file.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { TextService } from './text-service/text.service';
 import { FooterComponent } from './footer/footer.component';
+import { ApplyStyleFormatButtonComponent } from './format/components/apply-style-format-button/apply-style-format-button.component';
+import { ApplyReplaceFormatDropdownButtonComponent } from './format/components/apply-replace-format-dropdown-button/apply-replace-format-dropdown-button.component';
+import { FormatService } from './format/services/format.service';
+import { ControlTemplateDirective } from './shared/components/control-template.directive';
+import { IconButtonComponent } from './shared/components/icon-button/icon-button.component';
+import { DropdownComponent } from './shared/components/dropdown/dropdown.component';
+import { IconDropdownButtonComponent } from './shared/components/icon-dropdown-button/icon-dropdown-button.component';
+import { TextService } from './shared/services/text-service/text.service';
+import { SynonymsService } from './shared/services/synonyms/synonyms.service';
+import { RenderFormatComponentsService } from './format/services/render-format-components.service';
+
 
 @NgModule({
   declarations: [
@@ -18,11 +31,17 @@ import { FooterComponent } from './footer/footer.component';
     FileComponent,
     ControlPanelComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ControlTemplateDirective,
+    DropdownComponent,
+    IconButtonComponent,
+    IconDropdownButtonComponent,
+    ApplyStyleFormatButtonComponent,
+    ApplyReplaceFormatDropdownButtonComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     MatIconModule,
@@ -31,8 +50,15 @@ import { FooterComponent } from './footer/footer.component';
     MatSelectModule,
     MatOptionModule,
   ],
+  entryComponents: [
+    ApplyStyleFormatButtonComponent,
+    ApplyReplaceFormatDropdownButtonComponent,
+  ],
   providers: [
-    TextService
+    TextService,
+    FormatService,
+    SynonymsService,
+    RenderFormatComponentsService,
   ],
   bootstrap: [
     AppComponent
