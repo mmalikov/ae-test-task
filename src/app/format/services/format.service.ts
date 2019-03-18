75889d31ce225582405d89b562ebf5da39ba60d5
Michail Malykov <michail.malykov@gmail.com>
2019-03-18 15:55:13 +0800

    feat(format): add FormatService
    FormatService responsible for all format related action e.g. getting format, applying format .etc

---
+++ src/app/format/services/format.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { FORMAT_SETTINGS } from './format-settings';
+import { FORMAT, HANDLER_TYPES, IFormatSetting } from './format-types';
+import { getSelectionString, updateSelectionCss } from './format-utils';
+import { TextService } from '../../shared/services/text-service/text.service';
+import { SynonymsService } from '../../shared/services/synonyms/synonyms.service';
+
+@Injectable()
+export class FormatService {
+
+  private settings = FORMAT_SETTINGS;
+  private loadOptionsHandlers = {
+    [HANDLER_TYPES.REPLACE]: this.loadReplaceOptions.bind(this)
+  };
+
+  get selection() {
+    return this.textService.getSelection();
+  }
+
+  constructor(
+    private textService: TextService,
+    private synonymsService: SynonymsService,
+  ) {
+  }
+
+  getSettings(format) {
+    return this.settings[format];
+  }
+
+  updateSelectionFormat(setting: IFormatSetting, option?: string) {
+    switch (setting.handlerType) {
+      case HANDLER_TYPES.STYLE: {
+        return updateSelectionCss(this.selection, setting.cssClass);
+      }
+      case HANDLER_TYPES.REPLACE: {
+        return updateSelectionCss(this.selection, option);
+      }
+    }
+  }
+
+  getOptions() {
+    return this.synonymsService.options$;
+  }
+
+  loadFormatOptions(format: FORMAT) {
+    const settings = this.getSettings(format);
+    const handler = this.loadOptionsHandlers[settings.handlerType];
+
+    return handler(this.selection, settings);
+  }
+
+  private loadReplaceOptions(selection: Selection, settings?: IFormatSetting) {
+    const word: string = getSelectionString(selection);
+    this.synonymsService.load(word);
+  };
+}
