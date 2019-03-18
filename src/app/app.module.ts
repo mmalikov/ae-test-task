4ae0f8fa5feb342081421d25a4fc5ee5b1600e73
Michail Malykov <michail.malykov@gmail.com>
2019-03-18 15:37:21 +0800

    feat(core): connect and setup Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule, MatOptionModule, MatSelectModule, MatSnackBarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { FileComponent } from './file/file.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
@@ -19,6 +23,13 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatOptionModule,
   ],
   providers: [
     TextService
